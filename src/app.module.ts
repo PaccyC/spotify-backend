61013893caaa67ca78643f63cab00642c0c6f4da
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-30 00:45:08 +0200

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { SongsModule } from '../src/songs/songs.module';
 import { LoggerMiddleware } from 'logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SongsModule],
+  imports: [SongsModule, AuthModule],
   controllers: [],
   providers: [],
 })
