3ef0a8f41dcdbc43580309973a1e27a437f3f01f
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-29 16:38:51 +0200

    calling of service methods into a controllr

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,38 +1,39 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
-
+  constructor(private songsService:SongsService){}
 
    @Post()
    createSong(){
-     return {message:"New song created"}
+     return this.songsService.createSong()
    }
 
     @Get()
     findAll(){
-     return {message:"All songs are available"}
+     return this.songsService.getAllSongs();
     }
 
     // Getting a specific song
 
     @Get(':id')
     findOneSong(@Param('id') id: string){
-     return {message:"Song with id :"+id}
+     return this.findOneSong(id)
     }
 
     // Updating a song
 
     @Put(":id")
-    updateSong(@Param('id') id: string){
-     return {message:"Song with id :"+id+" updated"}
+    updateSong(@Param('id',ParseIntPipe) id: number){
+     return this.songsService.updateSong(id);
     }
 
     // deleting a song
 
     @Delete(":id")
-    deleteSong(@Param('id') id: string){
-     return {message:"Song with id :"+id+" deleted"}
+    deleteSong(@Param('id',ParseIntPipe) id: number){
+     return this.songsService.removeSong(id)
     }
 
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+
+
+    async createSong(){
+
+    }
+
+    // Getting all songs
+
+    async  getAllSongs(){
+
+    }
+    async  getOneSong(id: number){
+
+    }
+
+    // updating song information
+
+    async updateSong(id:number){
+
+    }
+
+    // remove song 
+
+    async removeSong(id:number){
+
+    }
+
+}
