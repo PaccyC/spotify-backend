1e1ccd3a3a4b8589f72131e8f6b038353229c663
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-30 17:35:53 +0200

    created sign in dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,31 @@ import { SongsModule } from './modules/songs/songs.module';
 import { LoggerMiddleware } from 'logger.middleware';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
 
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [SongsModule, AuthModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+
+   AuthModule, 
+  UserModule, 
+ TypeOrmModule.forRoot({
+  type:'mysql',
+  host:'127.0.0.1',
+  port:3306,
+  username:'test',
+  password:'Paccy@123456789',
+  database:'spotify_clone',
+  synchronize: true,
+ }),
+ PrismaModule,
+ 
+ SongsModule],
+
 })
 
 
