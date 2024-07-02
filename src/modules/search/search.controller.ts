36b5fef36d270c58fccbbadb7382d0e265dd4f89
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-02 09:00:14 +0200

    searching for songs,artists or playlists

--- src/modules/search/search.controller.ts
+++ src/modules/search/search.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { SearchService } from './search.service';
+import { SearchDto } from './dto/search.dto';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('search')
-export class SearchController {}
+export class SearchController {
+
+    constructor(private searchService:SearchService){}
+
+    @Get()
+    @UseGuards(JwtGuard)
+    search(@Query() dto:SearchDto){
+       return this.searchService.search(dto)
+    }
+
+}
